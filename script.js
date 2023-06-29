0eb6a14eccc752131938748d7e6df133494685a5
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-06-29 09:52:58 -0400

    Began building passwordGenerate function. Added prompt for how many characters and if capital letters are desired

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var numberCharacters = prompt("How many characters?")
+  if (numberCharacters === null) {
+    return;
+  }
+  else if (numberCharacters >128){
+    alert("Too many characters! Passwords must be 8 to 120 characters long." )
+    generatePassword();
+  }
+  else if (numberCharacters < 8){
+    alert("Not enough characters! Passwords must be 8 to 120 characters long.")
+    generatePassword();
+  }
+  capitalChoice =prompt("Do you want to use capital letters? type yes or no")
+  if (capitalChoice === null) {
+    return;
+  }
+  else if (capitalChoice == "yes") {
+    //write code to add upper case to array//
+  }
+  else if (capitalChoice != "no") {
+  alert("Invalid selection, please try again");
+  generatePassword();
 
+}
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
