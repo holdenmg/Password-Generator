6a34e0e2e530f373095efe03f3ee6026b50dddab
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-06-29 10:49:15 -0400

    Added lowercase question prompt. Added type checking logic to number of characters prompt

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 function generatePassword() {
   var numberCharacters = prompt("How many characters?")
-  if (numberCharacters === null) {
-    return;
-  }
-  else if (numberCharacters >128){
-    alert("Too many characters! Passwords must be 8 to 120 characters long." )
-    generatePassword();
-  }
-  else if (numberCharacters < 8){
-    alert("Not enough characters! Passwords must be 8 to 120 characters long.")
-    generatePassword();
-  }
+  
+    if (numberCharacters === null) {
+      return;
+    }
+    else if (numberCharacters >128){
+      alert("Too many characters! Passwords must be 8 to 120 characters long." )
+     return;
+    }
+    else if (numberCharacters < 8){
+      alert("Not enough characters! Passwords must be 8 to 120 characters long.")
+      return;
+    }
+    else if(isNaN(numberCharacters) != false ){
+    alert("Invalid selection, please try again");
+      return;
+    }
+
   capitalChoice =prompt("Do you want to use capital letters? type yes or no")
-  if (capitalChoice === null) {
-    return;
-  }
-  else if (capitalChoice == "yes") {
-    //write code to add upper case to array//
-  }
-  else if (capitalChoice != "no") {
-  alert("Invalid selection, please try again");
-  generatePassword();
+    if (capitalChoice === null) {
+      return;
+    }
+    else if (capitalChoice != "no" && capitalChoice != "yes") {
+      alert("Invalid selection, please try again");
+      return;
+    }
+
+  lowerChoice = prompt("Do you want to use lower case letters? type yes or no")
+    if (lowerChoice === null) {
+      return;
+    }
+    else if (lowerChoice != "no" && lowerChoice != "yes") {
+      alert("Invalid selection, please try again");
+      return;
+    }
+
 
 }
-}
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
